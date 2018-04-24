8139f484cf455d28d0cf81d6809bc74e91655fa5
Colton Williams <clwilliams8@ualr.edu>
2018-04-23 21:21:15 -0500

    Commit 10 - Added new View and projection matrixes and camera class

--- OpenGL_3D_Game/src/engineTester/MainGameLoop.java
+++ OpenGL_3D_Game/src/engineTester/MainGameLoop.java
@@ -1,69 +1,130 @@
 package engineTester;
-
-import org.lwjgl.opengl.Display;
-
+ 
 import models.RawModel;
 import models.TexturedModel;
+ 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+ 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
-
+import entities.Camera;
+import entities.Entity;
+ 
 public class MainGameLoop {
-
-	public static void main(String[] args) {
-		
-		DisplayManager.createDisplay();
-		
-		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
-		StaticShader shader = new StaticShader();
-		
-		//OpenGl wants these define counterclockwise
-		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,//v0
-				-0.5f, -0.5f, 0f,//v1
-				0.5f, -0.5f, 0f,//v2
-				0.5f, 0.5f, 0f,//v3
-		};
-		
-		int[] indices = {
-				0,1,3,//top left triangle (v0, v1, v3)
-				3,1,2//bottom right triangle (v3, v1, v2)
-		};
-		
-		float[] textureCoords = {
-				0,0,	//V0
-				0,1,	//V1
-				1,1,	//V2
-				1,0		//V3
-		};
-		
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("clay"));
-		TexturedModel texturedModel = new TexturedModel(model,texture);
-		
-		while(!Display.isCloseRequested()) {
-			
-			//game logic
-			
-			//render
-			renderer.prepare();
-			shader.start();
-			renderer.render(texturedModel);
-			shader.stop();
-			DisplayManager.updateDisplay();
-			
-			
-			
-		}
-		shader.cleanUp();
-		loader.cleanUP();
-		DisplayManager.closeDisplay();
-		
-	}
-
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+         
+        float[] vertices = {            
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                0.5f,-0.5f,0,   
+                0.5f,0.5f,0,        
+                 
+                -0.5f,0.5f,1,   
+                -0.5f,-0.5f,1,  
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                0.5f,0.5f,0,    
+                0.5f,-0.5f,0,   
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                -0.5f,-0.5f,1,  
+                -0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,1,
+                -0.5f,0.5f,0,
+                0.5f,0.5f,0,
+                0.5f,0.5f,1,
+                 
+                -0.5f,-0.5f,1,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1
+                 
+        };
+         
+        float[] textureCoords = {
+                 
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+ 
+                 
+        };
+         
+        int[] indices = {
+                0,1,3,  
+                3,1,2,  
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,   
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+ 
+        };
+         
+        RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+         
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("clay")));
+         
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+         
+        Camera camera = new Camera();
+         
+        while(!Display.isCloseRequested()){
+            entity.increaseRotation(1, 1, 0);
+            camera.move();
+            renderer.prepare();
+            shader.start();
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+ 
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
 }
